04442f5f81036f3cf90f47b49a9eb3ee8b7358cc
Denis Dokleja <denis.dokleja@emilfreydigital.hr>
2026-04-07 20:44:01 +0200

    update vscode to v1.100.0 and fix dependancy vulnerability

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ import { runCdCommand } from "./cdhere";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "kubeApply" is now active!');
+  console.log('Congratulations, your extension "kubeSafeApply" is now active!');
 
   let disposableApply = vscode.commands.registerCommand(
     "kubeApply.apply",
